d08664a72b4791e59dec661195eab0a5ab2726bf
MattDague <mattdague@gmail.com>
2019-07-14 19:57:08 -0400

    finished html/css and added beginning of javascript

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+    var firebaseConfig = {
+    apiKey: "AIzaSyCO3GWGFUQE5CjCYpAb346YB8OnibRRC-I",
+    authDomain: "mattsproject-54627.firebaseapp.com",
+    databaseURL: "https://mattsproject-54627.firebaseio.com",
+    projectId: "mattsproject-54627",
+    storageBucket: "mattsproject-54627.appspot.com",
+    messagingSenderId: "1015675607617",
+    appId: "1:1015675607617:web:ba6417b6a5bbac8f"
+  };
+  
