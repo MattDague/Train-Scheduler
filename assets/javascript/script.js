66a591bb8cbf0c166939ced8e6b3049cd89bc0d2
Matthew Dague <mattdague@gmail.com>
2019-07-15 12:38:50 -0400

    added connection to firebase and created variables

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,3 +8,51 @@
     appId: "1:1015675607617:web:ba6417b6a5bbac8f"
   };
   
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+
+  var train = "";
+  var destination = "";
+  var firstTrainTime = 0;
+  var frequency = 0;
+
+  $("#submitButton").on("click", function (event) {
+    event.preventDefault();
+
+
+    train = $("#inputName").val().trim();
+    destination = $("#inputDestination").val().trim();
+    firstTrainTime= $("#inputTrainTime").val();
+    frequency = $("#inputFrequency").val().trim();
+
+    var newTrain = {
+        train: train,
+        destination: destination,
+        firstTrainTime: firstTrainTime,
+        frequency: frequency
+    }
+    database.ref().push(newTrain);
+
+});
+
+// database.ref().on("child_added", function (snapshot) {
+//   var store = snapshot.val();
+//   var tRow = $("<tr>");
+//   var nameTd = $("<td>").text(store.name);
+//   var roleTd = $("<td>").text(store.role);
+//   var startTd = $("<td>").text(store.start);
+//   var rateTd = $("<td>").text("$"+ store.rate + ".00");
+//   var convertedDate = moment(store.start, "MM/DD/YYYY");
+//   var months=moment().diff(moment(store.start, "YYYY/MM/DD"), "months");
+//   var workedTd = $("<td>").text(months);
+//   var billedTd = $("<td>").text("$" + months*store.rate + ".00");
+//   tRow.append(nameTd, roleTd, startTd, workedTd, rateTd,billedTd);
+//   $("tbody").append(tRow);
+//   console.log(employeeName);
+//   console.log(convertedDate);
+//   console.log(months);
+//   console.log(store.start);
+
+
+
+})
