91004ac16f314396f91efd28cb4e786a3030eba9
MattDague <mattdague@gmail.com>
2019-07-15 21:18:50 -0400

    mostly final version, fully working and firebase response working. Added javascript formulas for the t able categories and adjusted some css.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Train-Scheduler
+
+Welcome to the Train Scheduler! This scheduler uses firebase as a database to store train information. By entering the information for any train you like you can see when it will next arrive and how many minutes away.
+
+This is all done by using the first train time and frequency and sending that information to firebase. When the site retrieves the information from the database it uses a couple of javascript formulas to display the relevant information regarding its next arrival. 
+
+Most of the time on this site is gathered and converted using moment.js.
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,58 +1,76 @@
-    var firebaseConfig = {
-    apiKey: "AIzaSyCO3GWGFUQE5CjCYpAb346YB8OnibRRC-I",
-    authDomain: "mattsproject-54627.firebaseapp.com",
-    databaseURL: "https://mattsproject-54627.firebaseio.com",
-    projectId: "mattsproject-54627",
-    storageBucket: "mattsproject-54627.appspot.com",
-    messagingSenderId: "1015675607617",
-    appId: "1:1015675607617:web:ba6417b6a5bbac8f"
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  var database = firebase.database();
+//firebase connection
+var firebaseConfig = {
+  apiKey: "AIzaSyCO3GWGFUQE5CjCYpAb346YB8OnibRRC-I",
+  authDomain: "mattsproject-54627.firebaseapp.com",
+  databaseURL: "https://mattsproject-54627.firebaseio.com",
+  projectId: "mattsproject-54627",
+  storageBucket: "mattsproject-54627.appspot.com",
+  messagingSenderId: "1015675607617",
+  appId: "1:1015675607617:web:ba6417b6a5bbac8f"
+};
+//initialize call to firebase
+firebase.initializeApp(firebaseConfig);
+//variable for firebase database
+var database = firebase.database();
 
-  var train = "";
-  var destination = "";
-  var firstTrainTime = 0;
-  var frequency = 0;
+var train = "";
+var destination = "";
+var firstTrainTime = 0;
+var frequency = 0;
+//on clikc function for form
+$("#submitButton").on("click", function (event) {
+  event.preventDefault();
 
-  $("#submitButton").on("click", function (event) {
-    event.preventDefault();
-
-
-    train = $("#inputName").val().trim();
-    destination = $("#inputDestination").val().trim();
-    firstTrainTime= $("#inputTrainTime").val();
-    frequency = $("#inputFrequency").val().trim();
-
-    var newTrain = {
-        train: train,
-        destination: destination,
-        firstTrainTime: firstTrainTime,
-        frequency: frequency
-    }
-    database.ref().push(newTrain);
+//gathers values from each of the input lines
+  train = $("#inputName").val().trim();
+  destination = $("#inputDestination").val().trim();
+  firstTrainTime = $("#inputTrainTime").val();
+  frequency = $("#inputFrequency").val().trim();
+//variable for a new train to be added to database
+  var newTrain = {
+    train: train,
+    destination: destination,
+    firstTrainTime: firstTrainTime,
+    frequency: frequency
+  }
+  //pushes the new trains information to the database
+  database.ref().push(newTrain);
+  // clears the boxes after getting the information
+  $("#inputName").val("");
+  $("#inputDestination").val("");
+  $("#inputTrainTime").val("");
+  $("#inputFrequency").val("");
 
 });
 
-// database.ref().on("child_added", function (snapshot) {
-//   var store = snapshot.val();
-//   var tRow = $("<tr>");
-//   var nameTd = $("<td>").text(store.name);
-//   var roleTd = $("<td>").text(store.role);
-//   var startTd = $("<td>").text(store.start);
-//   var rateTd = $("<td>").text("$"+ store.rate + ".00");
-//   var convertedDate = moment(store.start, "MM/DD/YYYY");
-//   var months=moment().diff(moment(store.start, "YYYY/MM/DD"), "months");
-//   var workedTd = $("<td>").text(months);
-//   var billedTd = $("<td>").text("$" + months*store.rate + ".00");
-//   tRow.append(nameTd, roleTd, startTd, workedTd, rateTd,billedTd);
-//   $("tbody").append(tRow);
-//   console.log(employeeName);
-//   console.log(convertedDate);
-//   console.log(months);
-//   console.log(store.start);
+database.ref().on("child_added", function (snapshot) {
+  //variable for database response
+  var store = snapshot.val();
+  // creates row to add to table later
+  var tRow = $("<tr>");
+  // adds train name text
+  var trainTd = $("<td>").text(store.train);
+  // adds train destination text
+  var destinationTd = $("<td>").text(store.destination);
+  // adds train frequency text
+  var frequencyTd = $("<td>").text("Every " + store.frequency + " min");
+  // converts time of the first train using momentjs
+  var timeConvert = moment(store.firstTrainTime, "HH:mm").subtract(1, "years");
+  // difference in time from first train to now
+  var diffTime = moment().diff(moment(timeConvert), "minutes");
+  // remaining minutes after dividing the difference and frequency
+  var tRemainder = diffTime % store.frequency;
+  // formula for getting the minutes until train arrives
+  var minutesTil = store.frequency - tRemainder
+  // adds minutes til train arrives text
+  var minutesAwayTd = $("<td>").text(minutesTil);
+  // minutes until next train arrives
+  var nextTrain = moment().add(minutesTil, "minutes");
+  // adds arrival time text
+  var nextArrivalTd = $("<td>").text(moment(nextTrain).format("hh:mm"));
+  // adds all tds to the row created
+  tRow.append(trainTd, destinationTd, frequencyTd, nextArrivalTd, minutesAwayTd);
+  // adds the new row to the table 
+  $("tbody").append(tRow);
 
-
-
-})
+});
